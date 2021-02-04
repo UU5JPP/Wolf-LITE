d4686b4abb8b73e2918c80b36ee6b11f3444bd4d
Антон <43981173+UU5JPP@users.noreply.github.com>
2021-02-04 13:11:13 +0300

    LCD timings

--- STM32/Core/Src/settings.h
+++ STM32/Core/Src/settings.h
@@ -34,11 +34,11 @@
 
 // select LCD, comment on others
 //#define LCD_ILI9481 true
-#define LCD_HX8357B true
+//#define LCD_HX8357B true
 //#define LCD_HX8357C true
-//#define LCD_ILI9486 true
+#define LCD_ILI9486 true
 
-#define SCREEN_ROTATE 2 // povorot displey 2,4
+#define SCREEN_ROTATE 4 // povorot displey 2,4
 
 //SPI Speed
 #define SPI_FRONT_UNIT_PRESCALER SPI_BAUDRATEPRESCALER_8
