18e590da81a3b59f67f6ddc3ea094794d3016862
XGudron <XGudron@gmail.com>
2021-05-31 23:48:00 +0300

    PWM frequency update

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -638,46 +638,6 @@ static void MX_TIM3_Init(void)
   * @param None
   * @retval None
   */
-//static void MX_TIM4_Init(void)
-//{
-
-//  /* USER CODE BEGIN TIM4_Init 0 */
-
-//  /* USER CODE END TIM4_Init 0 */
-
-//  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-//  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-//  /* USER CODE BEGIN TIM4_Init 1 */
-
-//  /* USER CODE END TIM4_Init 1 */
-//  htim4.Instance = TIM4;
-//  htim4.Init.Prescaler = 4199;
-//  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-//  htim4.Init.Period = 199;
-//  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-//  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-//  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-//  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
-//  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-//  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  /* USER CODE BEGIN TIM4_Init 2 */
-
-//  /* USER CODE END TIM4_Init 2 */
-
-//}
-
 static void MX_TIM4_Init(void) //LCD PWM
 {
 
@@ -693,7 +653,7 @@ static void MX_TIM4_Init(void) //LCD PWM
 
   /* USER CODE END TIM3_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 64-1;
+  htim4.Init.Prescaler = 32-1;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 500-1;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
