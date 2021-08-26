db247f577106682bfe96401a3ba719c517ed5709
Антон <43981173+UU5JPP@users.noreply.github.com>
2021-08-26 17:40:20 +0300

    Блокировка валкодера по TX

--- STM32/Core/Src/front_unit.c
+++ STM32/Core/Src/front_unit.c
@@ -259,6 +259,8 @@ void FRONTPANEL_ENCODER2_checkRotate(void)
 
 static void FRONTPANEL_ENCODER_Rotated(float32_t direction) // rotated encoder, handler here, direction -1 - left, 1 - right
 {
+	if (TRX_on_TX())
+		return;
 	if (TRX.Locked)
 		return;
 	if (LCD_systemMenuOpened)
